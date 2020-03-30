c73f0c990d6f5ba100b5035d1d65cf2ad57882a7
钟致远电脑服务 <58477698+zzysite@users.noreply.github.com>
2020-03-30 18:59:52 +0800

    Update fxxkDingtalkLike.py

--- fxxkDingtalkLike.py
+++ fxxkDingtalkLike.py
@@ -1,5 +1,5 @@
 # 导入pyautogui模块
 import pyautogui
 # 在clicks设置点击个数
-# interval设置时间间隔
-pyautogui.click(clicks=10000000, interval=0.001)
+# interval设置时间间隔（注：新版本会监测点赞速度，我亲测只能最小0.5）
+pyautogui.click(clicks=10000000, interval=0.5)
